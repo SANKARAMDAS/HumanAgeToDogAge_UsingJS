2621e9b3f1272b9867704f4aa136b7c487f2cad4
DSankaram <sankaram@octaloop.com>
2022-12-23 12:58:50 +0530

    Dogs mature at a faster rate than human beings. We often say a dog’s age can be calculated in “dog years” to account for their growth compared to a human of the same age. Here is the program for that

---
+++ hello.js
@@ -0,0 +1,9 @@
+const myAge = 26;
+let earlyYears = 2;
+earlyYears *= 10.5;
+let laterYears = myAge - 2 ;
+laterYears *= 4 ;
+//adding earlyAge and lateAge together
+let myAgeInDogYears = earlyYears + laterYears;
+myName = 'Abd Xyz'.toLowerCase();
+console.log(`My Name is ${myName}. I'm ${myAge} years old in human year which is ${myAgeInDogYears} years old in dog years.`);
